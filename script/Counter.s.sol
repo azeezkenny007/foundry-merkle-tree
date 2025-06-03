add5abb805dfdea9c469c922c700aff6113c865a
azeezkenny007 <azeezokhamena@gmail.com>
2025-06-03 12:33:18 +0100

    style(script): remove extra whitespace in Counter.s.sol
    Clean up code formatting by removing unnecessary blank line in Counter contract script.

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -5,12 +5,12 @@ import {Script, console} from "forge-std/Script.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
-    
     /**
      * @dev Counter contract instance
      * Used to deploy and interact with the Counter contract
      */
     Counter public counter;
+    
 
     function setUp() public {}
 
