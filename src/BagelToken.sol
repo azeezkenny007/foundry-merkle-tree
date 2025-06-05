a3b9c68bfde9674f064d1379f6d1bd47b1ec368a
azeezkenny007 <azeezokhamena@gmail.com>
2025-06-05 15:07:41 +0100

    feat: add BagelToken ERC20 contract and MerkleAirdrop stub
    - Add BagelToken contract implementing ERC20 with minting functionality
    - Add empty MerkleAirdrop contract as placeholder
    - Remove Counter contract and related test/script files
    - Update OpenZeppelin contracts submodule to latest version
    - Add remappings.txt for OpenZeppelin contracts path

---
+++ src/BagelToken.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title BagelToken
+ * @author okhamena azeez
+ * @notice This contract implements a basic ERC20 token with minting capabilities
+ * @dev Extends OpenZeppelin's ERC20 and Ownable contracts
+ */
+contract BagelToken is ERC20, Ownable {
+    /**
+     * @notice Contract constructor that initializes the token with name "BagelToken" and symbol "BGL"
+     */
+    constructor() ERC20("BagelToken", "BGL") Ownable(msg.sender) {}
+
+    /**
+     * @notice Allows the owner to mint new tokens
+     * @dev Only callable by the contract owner
+     * @param to The address that will receive the minted tokens
+     * @param amount The amount of tokens to mint
+     */
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
