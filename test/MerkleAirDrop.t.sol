0758d6cd0b7a26ded79c9e091f6ed2f45d09f8be
azeezkenny007 <azeezokhamena@gmail.com>
2025-06-12 06:41:18 +0100

    test(MerkleAirDrop): add setup and initial test case
    Add test setup with BagelToken and MerkleAirDrop initialization, including a basic test case for user claiming functionality.

--- test/MerkleAirDrop.t.sol
+++ test/MerkleAirDrop.t.sol
@@ -3,7 +3,22 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {MerkleAirDrop} from "../src/MerkleAirDrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
 
 contract MerkleAirDropTest is Test {
-    function setUp() public {}
+    MerkleAirDrop public merkleAirDrop;
+    BagelToken public bagelToken;
+    bytes32 public constant MERKLE_ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    address user;
+    uint256 userPrivateKey;
+
+    function setUp() public {
+        bagelToken = new BagelToken();
+        merkleAirDrop = new MerkleAirDrop(MERKLE_ROOT,bagelToken);
+        (user, userPrivateKey) = makeAddrAndKey("user");
+    }
+
+    function testUserCanClaim() external{
+        console.log("user address:",user);
+    }
 }
