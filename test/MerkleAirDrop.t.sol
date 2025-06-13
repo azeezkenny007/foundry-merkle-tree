c97f48521ce58624df6776282231f509170c69bc
azeezkenny007 <azeezokhamena@gmail.com>
2025-06-13 03:46:05 +0100

    refactor(MerkleAirdrop): use abi.encode instead of abi.encodePacked for leaf
    test(MerkleAirdrop): improve test coverage and readability
    
    - Replace abi.encodePacked with abi.encode for more consistent encoding
    - Add token minting and transfer in test setup
    - Include balance assertions in test case
    - Improve variable naming and structure in tests

--- test/MerkleAirDrop.t.sol
+++ test/MerkleAirDrop.t.sol
@@ -11,19 +11,27 @@ contract MerkleAirDropTest is Test {
     bytes32 public constant MERKLE_ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
     address user;
     uint256 userPrivateKey;
-    uint256 public constant AMOUNT= 25 * 1e18;
-    bytes32[] public PROOF = [0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a,0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576]; 
+    uint256 public constant AMOUNT_TO_CLAIM= 25 * 1e18;
+    uint256 public constant AMOUNT_TO_SEND = AMOUNT_TO_CLAIM * 4;
+    bytes32 proofOne =0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 proofTwo  = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] public PROOF = [proofOne, proofTwo]; 
    
     function setUp() external {
         bagelToken = new BagelToken();
         merkleAirDrop = new MerkleAirDrop(MERKLE_ROOT,bagelToken);
+        bagelToken.mint(bagelToken.owner(), AMOUNT_TO_SEND);
+        bagelToken.transfer(address(merkleAirDrop), AMOUNT_TO_SEND);
         (user, userPrivateKey) = makeAddrAndKey("user");
     }
 
-    function testUserCanClaim() external {
+    function testUserCanClaim() public {
         uint256 startingBalance = bagelToken.balanceOf(user);
         vm.prank(user);
-        merkleAirDrop.claim(user, AMOUNT, PROOF);
-
+        merkleAirDrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+        uint256 endingBalance =    bagelToken.balanceOf(user);
+        console.log("Ending Balance", endingBalance);
+        assertEq(endingBalance, startingBalance + AMOUNT_TO_CLAIM);
     }
+
 }
