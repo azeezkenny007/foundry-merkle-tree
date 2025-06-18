0d25b1f2e04ebbc226298504cb96b68e2f7df8d9
azeezkenny007 <azeezokhamena@gmail.com>
2025-06-18 07:08:24 +0100

    feat(MerkleAirdrop): add signature validation and airdrop claim structure
    - Introduced a new error for invalid signatures during token claims.
    - Added a struct to represent airdrop claims, including claimer address and amount.
    - Updated the claim function to include signature parameters and validate signatures.
    - Implemented a new function to generate the message hash for signature verification.

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -20,6 +20,13 @@ contract MerkleAirDrop {
     /// @custom:error Thrown when an address tries to claim tokens more than once.
     error MerkleAirdrop__AlreadyClaimed();
 
+    error MerkleAirdrop__InvalidSignature()
+
+     struct AirdropClaim{
+        address claimer;
+        uint256 amount;
+     }  
+
     /**
      * @notice Emitted when a user successfully claims their tokens.
      * @param claimer The address that claimed the tokens.
@@ -50,9 +57,13 @@ contract MerkleAirDrop {
      * @param _amount The amount of tokens to claim.
      * @param _merkleProof The Merkle proof required to verify the claimer's inclusion in the whitelist.
      */
-    function claim(address _claimer, uint256 _amount, bytes32[] calldata _merkleProof) external {
+    function claim(address _claimer, uint256 _amount, bytes32[] calldata _merkleProof,uint8 v, bytes32 r, bytes32 s) external {
         if (s_claimed[_claimer] == true) {
             revert MerkleAirdrop__AlreadyClaimed();
+        }
+
+        if(!_IsValidSignature(_claimer,getMessage(_claimer,amount),v,r,s)){
+            revert MerkleAirdrop__InvalidSignature();
         }
 
         bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(_claimer, _amount))));
@@ -70,6 +81,10 @@ contract MerkleAirDrop {
      */
     function getMerkleRoot() external view returns (bytes32) {
         return i_merkleRoot;
+    }
+
+    function getMessage(address _claimer,uint256 _amount) public view returns(bytes32){
+        return  _hashTypedDataV4(keccak256(abi.encode(keccak256(abi.encode(MESSAGE_TYPEHASH, _claimer, _amount)))));
     }
 
     /**
